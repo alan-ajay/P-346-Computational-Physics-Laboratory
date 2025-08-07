5e5d9e18c92492f3b7d46bd4de3fbc369d8c0c52
Alan Ajay <alan.ajay@niser.ac.in>
2025-08-07 17:07:55 +0530

    Assignment 0 completed

---
+++ Assignment0/my_lib.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class MyComplex():
+    
+    def __init__(self, real = 0, imag = 0.0):
+        self.r = real
+        self.i = imag
+
+    def display_cmplx(self):
+        print(f'{self.r} {self.i}j')
+
+    def add_cmplx(self, c1, c2):
+        self.r = c1.r + c2.r
+        self.i = c1.i + c2.i
+        return MyComplex(self)
+
+    def sub_cmplx(self, c1, c2):
+        self.r = c1.r - c2.r
+        self.i = c1.i - c2.i
+        return MyComplex(self)
+    
+    def mul_cmplx(self, c1, c2):
+        self.r = c1.r*c2.r - c1.i*c2.i
+        self.i = c1.i*c2.r + c1.r*c2.i
+        return MyComplex(self)
+    
+    def mod_cmplx(self):
+        return np.sqrt(self.r**2+self.i**2)
