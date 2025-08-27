f9f38464a8d349adabc4aef2703481057ce648d6
Alan Ajay <alan.ajay@niser.ac.in>
2025-08-27 17:10:00 +0530

    Mistake corrected

--- Assignment3/Submissions/mylib_3_1.py
+++ Assignment3/Submissions/mylib_3_1.py
@@ -1,63 +1,5 @@
 import numpy as np
 
-def LU_decompose(A):
-    n = len(A)
-    L, U = [[0 for _ in range(n)] for _ in range(n)], [[0 for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        L[i][i] = 1
-    for j in range(n):
-        U[0][j] = A[0][j]
-    for j in range(n):
-        for i in range(1, j+1):
-            sum = 0
-            for k in range(i):
-                sum += L[i][k]*U[k][j]
-            U[i][j] = A[i][j] - sum
-        for i in range(j, n):
-            sum = 0
-            for k in range(j):
-                sum += L[i][k]*U[k][j]
-            L[i][j] = (A[i][j] - sum)/U[j][j]   
-    return {'L' : L,
-            'U' : U}   
-
-
-def LU_solve(A, b):
-    n = len(A)
-    sol = LU_decompose(A)
-    L = sol['L']
-    U = sol ['U']
-    A = []
-    y = []
-
-    for i in range(n):
-        sum = 0
-        for k in range(i):
-            sum += L[i][k]*y[k]
-        yi = b[i] - sum
-        y.append(yi)
-    x = [0 for _ in range(n)]
-    for i in range(n-1, -1, -1):
-        sum = 0
-        for k in range(i+1, n):
-            sum += U[i][k]*x[k]
-        x[i] = (y[i] - sum)/U[i][i]
-    return x
-
-def matrix_multiply(A, B):
-    N = len(A)
-    AB = []
-    for i in range(N):
-        L = []
-        for j in range(N):
-            sum = 0
-            
-            for k in range(N):
-                sum += A[i][k]*B[k][j]
-            L.append(round(sum, 1))    
-        AB.append(L) 
-    return AB
-
 def Cholesky_decomposition(A):
     n = len(A)
     L = [[0 for i in range(n)] for i in range(n)]
@@ -71,7 +13,28 @@ def Cholesky_decomposition(A):
             for k in range(i):
                 sum2+=L[k][i]*L[k][j]
             L[i][j] = (A[i][j] - sum2)/(L[i][i])        
-    return L   
+    return L  
+
+def Cholesky_solve(A, b):
+    n = len(A)
+    U = Cholesky_decomposition(A)
+    L = Transpose(U)
+    A = []
+    y = []
+
+    for i in range(n):
+        sum = 0
+        for k in range(i):
+            sum += L[i][k]*y[k]
+        yi = (b[i] - sum)/L[i][i]
+        y.append(yi)
+    x = [0 for _ in range(n)]
+    for i in range(n-1, -1, -1):
+        sum = 0
+        for k in range(i+1, n):
+            sum += U[i][k]*x[k]
+        x[i] = (y[i] - sum)/U[i][i]
+    return x
 
 def Transpose(A):
     n = len(A)
@@ -85,7 +48,9 @@ def Jacobi_iter(A, b, e = 1e-6, x = None):
     n = len(A)
     Stop  = False
     x = [0 for _ in range(n)]
+    count = 0
     while Stop  == False:
+        count += 1
         xi = x.copy()
         for i in range(n):
             sum = 0
@@ -93,10 +58,10 @@ def Jacobi_iter(A, b, e = 1e-6, x = None):
                 if j != i:
                     sum+= A[i][j]*xi[j]
             x[i] = (b[i]-sum)/A[i][i]
-        count = 0    
+        check = 0    
         for x1,x2 in zip(xi, x):
             if abs(x1-x2) > e:
-                count += 1
-        if count == 0:
+                check += 1
+        if check == 0:
             Stop = True                 
-    return x      
+    return x, count     
